f84f8c640ffb99a706b63e19b60d6b8435188ec5
Mario Torres <scaramuzzini@gmail.com>
2024-06-03 15:34:35 -0400

    Captura do nome digitado e exibição

--- app/src/main/java/com/oceanbrasil/ocean202406introparte1/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean202406introparte1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.oceanbrasil.ocean202406introparte1
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,10 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button2 = findViewById<Button>(R.id.button2)
+        val nomeDoUsuario = findViewById<EditText>(R.id.nomeDoUsuario)
+        val nomeDigitado = findViewById<TextView>(R.id.nomeDigitado)
 
         button2.setOnClickListener {
-            Log.d("oceanbrasil", "O texto do botao e:"+button2.text.toString())
-            Toast.makeText(this, "Clicou!", Toast.LENGTH_LONG).show()
+            //Log.d("oceanbrasil", "O texto do botao e:"+button2.text.toString())
+            Toast.makeText(this, nomeDoUsuario.text, Toast.LENGTH_LONG).show()
+            val nome = nomeDoUsuario.text.toString()
+            nomeDigitado.text = "O nome digitado foi: $nome"
         }
     }
 }
